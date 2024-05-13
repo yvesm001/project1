4fb7f5007241c4d0691c944fe2489de2361dc280
Yves Martinez <yves.oliver.m@gmail.com>
2024-05-13 09:23:30 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+
+    setGame();
+
+
+
+
+
+
+    const playButton = document.getElementById('play-button');
+
+    playButton.addEventListener("click", function () {
+        console.log("Starting new game");
+    })
+}
